47a4f972c5ef39f4bf60067d3241f58cafe40453
jeremiasiturriza <jeremias.nestor@gmail.com>
2024-03-23 17:14:32 -0300

    first commit

---
+++ src/com/miorganizacion/MiClase.java
@@ -0,0 +1,12 @@
+package com.miorganizacion;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MiClase {
+    public static void main(String[] args) {
+        ArrayList<String> lista = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        // Utiliza ArrayList y Scanner aquí
+    }
+}
